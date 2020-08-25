77a45cf0ed08948d174e49109e3c30b6b3c26278
Isaí Alejandro <isaialejandro26@gmail.com>
2020-08-25 18:07:46 -0500

    .

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -6,6 +6,7 @@ const preferDefault = m => (m && m.default) || m
 
 exports.components = {
   "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/isaialejandro/Documents/workSpace/Gatsby/Portfolio/isai-portfolio/.cache/dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("/Users/isaialejandro/Documents/workSpace/Gatsby/Portfolio/isai-portfolio/src/pages/404.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/isaialejandro/Documents/workSpace/Gatsby/Portfolio/isai-portfolio/src/pages/index.js")))
 }
 
